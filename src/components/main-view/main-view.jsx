48af1ef2e941072603a4e0fc146dede0c0582b06
karenlyyys <99241654+karenlyyys@users.noreply.github.com>
2022-06-03 14:22:54 -0700

    edited for 3.3

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -37,6 +37,7 @@ import '../../../src/index';
       
         return (
           <div className="main-view">
+            <button onClick={() => {alert('Nice!')}}>Click me!</button>
             {movies.map(movie => <MovieCard key={movie._id} movie={movie}/>)}
           </div>
         );
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,14 +2,11 @@ import React from 'react';
 import { MovieView } from '../movie-view/movie-view';
 
 export class MovieCard extends React.Component {
+ 
   render() {
-    const { movies } = this.props;
-    return <div className="movie-card" >{
+    const { movie, onMovieClick } = this.props;
 
-      movies.map(movie => <MovieView key={movie._id} movie={movie}/>)
-
-    }</div>;
+    return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>;
   }
-}
 
-{}
+}

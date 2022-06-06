10163d703b4d888577210a7cdb3da88c2108a99c
karenlyyys <99241654+karenlyyys@users.noreply.github.com>
2022-06-05 21:20:56 -0700

    edited for 3.3

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import { MovieView } from '../movie-view/movie-view';
+import Button from 'react-bootstrap/Button';
+
+import "./movie-view.scss"
+
+import { Card, Col, Container, Row, Button } from "react-bootstrap";
 
 export class MovieCard extends React.Component {
  

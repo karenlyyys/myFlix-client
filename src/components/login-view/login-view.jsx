882cff2a6d9559bec0fbe99e81f6987db3739f64
karenlyyys <99241654+karenlyyys@users.noreply.github.com>
2022-06-20 00:44:59 -0700

    implemented hooks

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -5,11 +5,39 @@ import { Button } from 'react-bootstrap';
 import "../../index.scss"
 
 export class Login extends React.Component {
+  constructor(props) {
+    super(props);
  
-    state = {
+    this.state = {
         username: '',
         password: ''
-    }
+    };
+
+    this.onUsernameChange = this.onUsernameChange.bind(this);
+    this.onPasswordChange = this.onPasswordChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  onUsernameChange(event) {
+    this.setState({
+      username: event.target.value
+    });
+  }
+
+  onPasswordChange(event) {
+    this.setState({
+      password: event.target.value
+    });
+  }
+
+  handleSubmit() {
+    const { username, password } = this.state;
+    console.log(username, password);
+    /* Send a request to the server for authentication */
+    /* then call this.props.onLoggedIn(username) */
+    // this.props.onLoggedIn(username);
+  }
+
   
   render() {
     const { login } = this.props;

c9ba227ff949e35c13788a61459cedd40ae0b5e2
karenlyyys <99241654+karenlyyys@users.noreply.github.com>
2022-06-20 11:22:11 -0700

    added hooks

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,65 +1,116 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Button } from 'react-bootstrap';
 
 
 import "../../index.scss"
 
-export class Login extends React.Component {
-  constructor(props) {
-    super(props);
+// export class Login extends React.Component {
+//   constructor(props) {
+//     super(props);
  
-    this.state = {
-        username: '',
-        password: ''
-    };
+//     this.state = {
+//         username: '',
+//         password: ''
+//     };
 
-    this.onUsernameChange = this.onUsernameChange.bind(this);
-    this.onPasswordChange = this.onPasswordChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+//     this.onUsernameChange = this.onUsernameChange.bind(this);
+//     this.onPasswordChange = this.onPasswordChange.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
 
-  onUsernameChange(event) {
-    this.setState({
-      username: event.target.value
-    });
-  }
+//   onUsernameChange(event) {
+//     this.setState({
+//       username: event.target.value
+//     });
+//   }
 
-  onPasswordChange(event) {
-    this.setState({
-      password: event.target.value
-    });
-  }
+//   onPasswordChange(event) {
+//     this.setState({
+//       password: event.target.value
+//     });
+//   }
 
-  handleSubmit() {
-    const { username, password } = this.state;
-    console.log(username, password);
-    /* Send a request to the server for authentication */
-    /* then call this.props.onLoggedIn(username) */
-    // this.props.onLoggedIn(username);
-  }
+//   handleSubmit() {
+//     const { username, password } = this.state;
+//     console.log(username, password);
+//     /* Send a request to the server for authentication */
+//     /* then call this.props.onLoggedIn(username) */
+//     // this.props.onLoggedIn(username);
+//   }
 
   
-  render() {
-    const { login } = this.props;
+//   render() {
+//     const { login } = this.props;
 
+//     return (
+//       <div className="movie-card">
+        
+//         <div class="container">
+//         <label for="uname"><b>Username</b></label>
+//         <input type="text" placeholder="Enter Username" name="uname" required 
+//         onChange={(evt)=> this.setState({username: evt.target.value}) } />
+
+//         <label for="psw"><b>Password</b></label>
+//         <input type="password" placeholder="Enter Password" name="psw" required
+//          onChange={(evt)=> this.setState({password: evt.target.value}) }
+//          />
+//       </div>
+
+//         <Button className="movie-button primary" onClick={() => { login(this.state.username, this.state.password) }}>Log in</Button>
+//         </div>
+//     );
+//     // return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>;
+//   }
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export function Login (props){
+  const {onLoggedIn, login } = props;
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit() {
+    /* Send a request to the server for authentication */
+    /* then call this.props.onLoggedIn(username) */
+    onLoggedIn(username);
+  }
     return (
       <div className="movie-card">
         
         <div class="container">
         <label for="uname"><b>Username</b></label>
         <input type="text" placeholder="Enter Username" name="uname" required 
-        onChange={(evt)=> this.setState({username: evt.target.value}) } />
+        onChange={(evt)=> setUsername(evt.target.value) } />
 
         <label for="psw"><b>Password</b></label>
         <input type="password" placeholder="Enter Password" name="psw" required
-         onChange={(evt)=> this.setState({password: evt.target.value}) }
+         onChange={(evt)=> setPassword(evt.target.value) }
          />
       </div>
 
-        <Button className="movie-button primary" onClick={() => { login(this.state.username, this.state.password) }}>Log in</Button>
+        <Button className="movie-button primary" onClick={() => { login(username, password) }}>Log in</Button>
         </div>
     );
     // return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>;
   }
-
-}
